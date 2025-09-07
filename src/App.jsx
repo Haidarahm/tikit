4b80e72197916e25014372c360ef636bc4b83898
HaidarAhmad123 <haidarahmad421@gmail.com>
2025-09-07 03:55:53 -0700

    Refactor App component structure and add Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import { Route, Routes } from "react-router-dom";
 import { Suspense, lazy } from "react";
 import Intro from "./components/Intro";
+import Navbar from "./components/Navbar";
 
 // Lazy load the Home component to trigger Suspense
 const Home = lazy(() => import("./pages/Home/Home"));
 
 function App() {
   return (
-    <Suspense fallback={<div>loading...</div>}>
-      <Routes>
-        <Route path="/" element={<Intro />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
-    </Suspense>
+    <div className="relative">
+      
+      <Suspense fallback={<div>loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Intro />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </Suspense>
+    </div>
   );
 }
 
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,21 +1,34 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Intro() {
   const navigate = useNavigate();
+  const [scaled, setScaled] = useState(false);
+  const videoRef = useRef(null);
 
   const handleVideoEnd = () => {
-    navigate("/home"); // Go to Home after video ends
+    setScaled(true); // Start scaling down
   };
+
+  const handleTransitionEnd = () => {
+    if (scaled) {
+      navigate("/home");
+    }
+  };
+
   return (
-    <div className="fixed  w-full h-dvh bg-white flex justify-center items-center">
+    <div className="fixed w-full h-dvh bg-white flex justify-center items-center">
       <video
+        ref={videoRef}
         src="/intro.mp4"
-        className="h-[200px]"
+        className={`h-[200px] transition-transform duration-500 ${
+          scaled ? "scale-0" : "scale-100"
+        }`}
         autoPlay
         muted
         playsInline
-        onEnded={handleVideoEnd} // Trigger navigation
+        onEnded={handleVideoEnd}
+        onTransitionEnd={handleTransitionEnd}
       ></video>
     </div>
   );
