c66c37a5111211b15239a678698a62fd6d07ce95
HaidarAhmad123 <haidarahmad421@gmail.com>
2025-09-08 00:56:17 -0700

    Refactor Home component layout for improved text positioning and remove unnecessary whitespace in CSS

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,9 +4,11 @@ import AuroraBackground from "../../components/AuroraBackground";
 
 function Home() {
   return (
-    <div className="w-full h-screen  text-white">
-      <AuroraBackground className="w-full h-screen"/>
-      Haidar Ibrahim
+    <div className="relative w-full h-screen text-white">
+      <AuroraBackground className="w-full h-screen" />
+      <div className="relative z-10 flex items-center justify-center h-full">
+        Haidar Ibrahim
+      </div>
     </div>
   );
 }
