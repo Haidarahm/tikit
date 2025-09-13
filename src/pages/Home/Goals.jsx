960fd58a88c09241667bf13e15eb48742471cd31
HaidarAhmad123 <haidarahmad421@gmail.com>
2025-09-13 05:12:45 -0700

    Refactor Goals component layout for improved structure; remove unnecessary ScrollStack items and adjust CSS class for better responsiveness.

--- src/pages/Home/Goals.jsx
+++ src/pages/Home/Goals.jsx
@@ -1,23 +1,13 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import ScrollStack, { ScrollStackItem } from "../../components/ScrollStackItem";
 
 function Goals() {
   return (
-    <div className=" section font-hero-light flex flex-col mx-auto  h-[calc(100%-104px)] mt-[104px]  z-10 w-6/7 justify-center">
-      <ScrollStack className="h-full w-full">
-        <ScrollStackItem>
-          <h2>Card 1</h2>
-          <p>This is the first card in the stack</p>
-        </ScrollStackItem>
-        <ScrollStackItem>
-          <h2>Card 2</h2>
-          <p>This is the second card in the stack</p>
-        </ScrollStackItem>
-        <ScrollStackItem>
-          <h2>Card 3</h2>
-          <p>This is the third card in the stack</p>
-        </ScrollStackItem>
-      </ScrollStack>
+    <div
+    
+      className="section font-hero-light flex flex-col mx-auto h-screen z-10 w-6/7 justify-center"
+    >
+  
     </div>
   );
 }
