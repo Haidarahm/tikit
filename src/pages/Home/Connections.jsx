ffc809174634adf3b23a99a3a327d4d40bad1ba4
HaidarAhmad123 <haidarahmad421@gmail.com>
2025-09-14 09:16:27 -0700

    Update Connections component: modify width styling for improved layout consistency and responsiveness.

--- src/pages/Home/Connections.jsx
+++ src/pages/Home/Connections.jsx
@@ -6,7 +6,7 @@ import card1Image3 from "../../assets/images/card-3.jpg";
 
 export const Connections = () => {
   return (
-    <div className="section relative font-hero-light flex items-center mx-auto justify-between min-h-screen w-[95vw] rounded-2xl">
+    <div className="section relative font-hero-light flex items-center mx-auto justify-between min-h-screen w-6/7   rounded-2xl">
       <div className="text w-1/2 relative z-10">
         <h1 className="title font-bold text-[48px] text-white">
           Bold, data-led <br /> marketing <br /> crafted by experts
