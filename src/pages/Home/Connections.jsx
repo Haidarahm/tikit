ffe3607916bd8732b928621abc73447a487e9271
HaidarAhmad123 <haidarahmad421@gmail.com>
2025-09-19 00:40:31 -0700

    fix

--- src/pages/Home/Connections.jsx
+++ src/pages/Home/Connections.jsx
@@ -3,14 +3,14 @@ import ScrollFloat from "../../components/ScrollFloat";
 
 const Connections = () => {
   return (
-    <div className="section h-[70vh] font-hero-light rounded-[25px] text-center w-[60vw] overflow-hidden mx-auto">
+    <div className="section h-[80vh] flex flex-col justify-center font-hero-light rounded-[25px] text-center w-[60vw] overflow-hidden mx-auto">
       <ScrollFloat
         animationDuration={1}
         ease="back.inOut(2)"
         textClassName="text-[60px] font-bold leading-[60px]"
-        scrollStart="center bottom+=50%"
-        scrollEnd="bottom bottom-=40%"
-        stagger={0.05}
+        scrollStart="center bottom+=20%"
+        scrollEnd="bottom bottom-=50%"
+        stagger={0.06}
       >
         Bold, Data-Led Marketing Crafted By Experts
       </ScrollFloat>
