46623238b1a4cc070e0fbecc24b1e23586d7d0dc
HaidarAhmad123 <haidarahmad421@gmail.com>
2025-09-22 03:42:01 -0700

    Refactor Connections component: restructure layout for improved readability and maintainability, ensuring consistent styling and spacing.

--- src/pages/Home/Connections.jsx
+++ src/pages/Home/Connections.jsx
@@ -20,34 +20,36 @@ const Connections = () => {
     });
   }, []);
   return (
-    <div className="section h-[100vh] flex flex-col justify-center font-hero-light rounded-[25px] text-center w-[60vw] overflow-hidden mx-auto mt-[60px]">
-      <ScrollFloat
-        animationDuration={1}
-        ease="back.inOut(2)"
-        textClassName="text-[64px] font-bold max-w-[600px] leading-[60px]"
-        scrollStart="center bottom+=20%"
-        scrollEnd="bottom bottom-=50%"
-        stagger={0.06}
-      >
-        Bold, Data-Led Marketing Crafted By Experts
-      </ScrollFloat>
+    <div className="section h-[100vh] flex flex-col justify-center font-hero-light  overflow-hidden  mt-[60px]">
+      <div className="flex flex-col justify-center w-[60vw]  mx-auto text-center">
+        <ScrollFloat
+          animationDuration={1}
+          ease="back.inOut(2)"
+          textClassName="text-[64px] font-bold max-w-[600px] leading-[60px]"
+          scrollStart="center bottom+=20%"
+          scrollEnd="bottom bottom-=50%"
+          stagger={0.06}
+        >
+          Bold, Data-Led Marketing Crafted By Experts
+        </ScrollFloat>
 
-      <p className="description text-[32px] font-light leading-[35px] mt-[20px]">
-        {splitText(
-          "blend data with creativity to help brands reach and resonate with the right audience."
-        ).map((word, index) => (
-          <span
-            key={index}
-            data-aos="fade-up"
-            data-aos-duration="600"
-            data-aos-delay={`${200 + index * 50}`}
-            data-aos-easing="ease-out-cubic"
-            className="inline-block mr-2"
-          >
-            {word}
-          </span>
-        ))}
-      </p>
+        <p className="description text-[32px] font-light leading-[35px] mt-[20px]">
+          {splitText(
+            "blend data with creativity to help brands reach and resonate with the right audience."
+          ).map((word, index) => (
+            <span
+              key={index}
+              data-aos="fade-up"
+              data-aos-duration="600"
+              data-aos-delay={`${200 + index * 50}`}
+              data-aos-easing="ease-out-cubic"
+              className="inline-block mr-2"
+            >
+              {word}
+            </span>
+          ))}
+        </p>
+      </div>
     </div>
   );
 };
