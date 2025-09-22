384e987db66c5a922f2b1389dbc8037d7a7a9886
HaidarAhmad123 <haidarahmad421@gmail.com>
2025-09-22 13:41:13 -0700

    Enhance ContactUs component: update layout for better responsiveness, integrate FloatingInput components for user input, and improve overall styling for a more engaging user experience.

--- src/pages/Home/ContactUs.jsx
+++ src/pages/Home/ContactUs.jsx
@@ -1,19 +1,39 @@
 import React from "react";
+import FloatingInput from "../../components/ui/FloatingInput";
 
 const ContactUs = () => {
   return (
-    <div className="section h-[70vh] font-hero-light rounded-[25px] flex flex-col overflow-hidden mx-auto  w-[95vw] bg-black">
-      <div className="email w-1/2">
-        <div className="texts flex flex-col">
+    <div className="relative text-white min-h-[100vh] font-hero-light rounded-[25px] flex flex-col mx-auto py-[60px] px-[50px]  w-[95vw] bg-black">
+      <div className="email w-full flex h-1/2">
+        <div className="texts flex  flex-col relative ">
           <h3 className="subtitle text-[50px]">Kick it off with Tikit!</h3>
           <h1 className="title text-[70px] font-bold">contact us now</h1>
-          <p className="description text-[24px] font-light">
+          <p className="description text-[24px] font-light w-full">
             We want to hear from you. let’s us know <br /> how we can help!
           </p>
         </div>
-        <div className="action w-1/2"></div>
+        <div className="action h-full flex-col flex justify-between">
+          <div className="title text-[24px]">
+            let’s us know how we can help!
+          </div>
+          <div className="inputs flex justify-between gap-[20px]">
+            <FloatingInput
+              id="contact-standard"
+              label="name"
+              containerClassName="mt-8 flex-1"
+            />
+            <FloatingInput
+              id="contact-standard"
+              label="email"
+              containerClassName="mt-8 flex-1"
+            />
+          </div>
+          <button className="w-full bg-[#303134] rounded-full py-[15px] text-[20px]">
+            Contact Us
+          </button>
+        </div>
       </div>
-      <div className="brands"></div>
+      <div className="brands h-1/2"></div>
     </div>
   );
 };
