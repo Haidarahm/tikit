09f247d7e2f2bdaa86c40b3d020319f3f9ea7d2d
HaidarAhmad123 <haidarahmad421@gmail.com>
2025-09-22 03:47:21 -0700

    Add animations to Connections component: integrate GSAP for element animations on scroll, enhancing visual engagement with new decorative elements.

--- src/pages/Home/Connections.jsx
+++ src/pages/Home/Connections.jsx
@@ -2,6 +2,11 @@ import React, { useEffect } from "react";
 import ScrollFloat from "../../components/ScrollFloat";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import element1 from "../../assets/elements/5.png";
+import element2 from "../../assets/elements/6.png";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
 // Helper function to split text into words
 const splitText = (text) => {
@@ -10,6 +15,42 @@ const splitText = (text) => {
 
 const Connections = () => {
   // Initialize AOS
+  useEffect(() => {
+    // Animate Element 1
+    gsap.to(".element1", {
+      top: "900px",
+      left: "1000px",
+      filter: "grayscale(100%)",
+      rotation: 100,
+      duration: 1.5,
+      ease: "power1.inOut",
+      scrollTrigger: {
+        start: "top 0%",
+        end: "bottom 0%",
+        scrub: 1.5,
+      },
+    });
+
+    // Animate Element 2
+    gsap.to(".element2", {
+      top: "90vh",
+      right: "70%",
+      filter: "grayscale(100%)",
+      rotation: 100,
+      duration: 1.5,
+      ease: "power1.inOut",
+      scrollTrigger: {
+        start: "top 0%",
+        end: "bottom 0%",
+        scrub: 1.5,
+      },
+    });
+
+    // Cleanup on unmount
+    return () => {
+      ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
+    };
+  }, []);
   useEffect(() => {
     AOS.init({
       duration: 1000,
@@ -20,7 +61,18 @@ const Connections = () => {
     });
   }, []);
   return (
-    <div className="section h-[100vh] flex flex-col justify-center font-hero-light  overflow-hidden  mt-[60px]">
+    <div className=" relative section h-[100vh] flex flex-col w-full justify-center font-hero-light section-container-scroll  overflow-hidden  mt-[60px]">
+       <img
+        src={element2}
+        alt="Decorative element 1"
+        className="element1 absolute top-4 left-8 z-10 w-auto h-auto max-w-[300px] max-h-[300px]"
+      />
+      {/* Element 2 */}
+      <img
+        src={element1}
+        alt="Decorative element 2"
+        className="element2 absolute top-[55vh] right-12 rotate-90 z-10 w-auto h-auto max-w-[300px] max-h-[300px]"
+      />
       <div className="flex flex-col justify-center w-[60vw]  mx-auto text-center">
         <ScrollFloat
           animationDuration={1}
