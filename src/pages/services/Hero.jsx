ab563593b907ed03479c8b56019fac643889bf98
HaidarAhmad123 <haidarahmad421@gmail.com>
2025-09-26 02:53:31 -0700

    fix

--- src/pages/services/Hero.jsx
+++ src/pages/services/Hero.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const Hero = () => {
-  return <div data-scroll-section className="h-screen  flex flex-col"></div>;
+  return (
+    <div
+      data-scroll-section
+      className="h-screen font-hero-light text-white justify-center items-center  flex flex-col"
+    >
+      <div className="texts mt-12 text-center">
+        <h1 className="text-[96px] leading-[100px] mb-8 capitalize">
+          {" "}
+          We make design that <br />{" "}
+          <span className="font-bold">lead and inspire</span>
+        </h1>
+        <p className="text-[32px]">
+          Whether we’re crafting a campaign or building a brand,
+          <br /> our approach is rooted in insight, storytelling, and <br />{" "}
+          execution that works.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
