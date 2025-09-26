35f42a878a940857760991b6daec2c08dda86eb4
HaidarAhmad123 <haidarahmad421@gmail.com>
2025-09-26 02:31:23 -0700

    FIX

--- src/pages/services/Details.jsx
+++ src/pages/services/Details.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Details = () => {
-  return (
-    <div className='h-screen'>
-      
-    </div>
-  )
-}
+  return <div className="h-screen"></div>;
+};
 
-export default Details
+export default Details;
--- src/pages/services/Hero.jsx
+++ src/pages/services/Hero.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
-  return (
-    <div data-scroll-section className="h-screen  flex flex-col">
-      
-    </div>
-  )
-}
+  return <div data-scroll-section className="h-screen  flex flex-col"></div>;
+};
 
-export default Hero
+export default Hero;
