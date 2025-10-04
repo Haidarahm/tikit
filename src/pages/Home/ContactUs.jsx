be19d306d72d8d942780b1fae68298894e2750a9
Haidar <hadiarahmad421@gmail.com>
2025-10-04 14:54:38 -0700

    Enhance ContactUs and Action components: integrate state management for slide functionality in ContactUs, adjust layout and styles for improved responsiveness, and refine button interactions in Action for better user experience.

--- src/pages/Home/ContactUs.jsx
+++ src/pages/Home/ContactUs.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import FloatingInput from "../../components/ui/FloatingInput";
 import Hyperspeed from "../../components/Hyperspeed";
 import LogoLoop from "../../components/LogoLoop";
@@ -10,8 +10,13 @@ import b3 from "../../assets/brands/3.svg";
 import b4 from "../../assets/brands/4.svg";
 import b5 from "../../assets/brands/5.svg";
 
-
 const ContactUs = (className) => {
+  const [isSecondSlide, setIsSecondSlide] = useState(false);
+
+  const handleSlideClick = (slideNumber) => {
+    setIsSecondSlide(slideNumber === 2);
+  };
+
   useEffect(() => {
     AOS.init({ duration: 750, once: true });
   }, []);
@@ -39,46 +44,72 @@ const ContactUs = (className) => {
     { src: b5, alt: "Brand 5" },
   ];
   return (
-    <div className={(`relative my-10 gap-3.5  overflow-hidden text-white  font-hero-light rounded-[25px] flex flex-col mx-auto py-[60px] px-[50px]  w-[95vw] bg-black ${className}`)}>
+    <div
+      className={`relative my-10 gap-3.5  overflow-hidden text-white  font-hero-light rounded-[25px] flex flex-col mx-auto py-[40px] md:py-[60px] px-[40px] md:px-[50px]  w-[95vw] bg-black ${className}`}
+    >
       <div className="overlay absolute w-full h-full bg-[#00000076] z-10"></div>
-      
-      <div className="email  w-full flex h-2/3 justify-between relative z-10 ">
+
+      <div className="email  w-full flex flex-col md:flex-row h-2/3 justify-between items-center md:items-stretch relative z-10 ">
         <div className="texts flex justify-between flex-col relative   ">
           <h3
-            className="subtitle text-[50px]"
+            className="subtitle text-center md:text-start text-[16px] md:text-[50px]"
             data-aos="fade-up"
             data-aos-delay="0"
           >
             Kick it off with Tikit!
           </h3>
           <h1
-            className="title text-[70px] font-bold"
+            className="title text-[32px] text-center md:text-start md:text-[70px] font-bold"
             data-aos="fade-up"
             data-aos-delay="150"
           >
             contact us now
           </h1>
           <p
-            className="description text-[24px] font-light w-full"
+            className="description hidden md:block text-[16px] md:text-[24px] font-light w-full"
             data-aos="fade-up"
             data-aos-delay="300"
           >
-            We want to hear from you. let’s us know <br /> how we can help!
+            We want to hear from you. let’s us know{" "}
+            <br className="hidden md:block" /> how we can help!
           </p>
         </div>
         <div
-          className="action w-[45%]  flex-col flex justify-between"
+          className="action w-full md:w-[45%] gap-[20px] md:gap-0 flex-col flex justify-between"
           data-aos="fade-left"
           data-aos-delay="0"
         >
-          <div className="title text-[24px]">
+          <div className="title text-center font-light md:font-medium md:text-start text-[20px] md:text-[24px]">
             let’s us know how we can help!
+          </div>
+          <div className="swiper-wrapper w-full border flex relative border-white h-[50px] rounded-full">
+            <div
+              className={`move-item absolute w-1/2 h-full bg-white rounded-full transition-all duration-300 ease-in-out ${
+                isSecondSlide ? "left-1/2" : "left-0"
+              }`}
+            />
+            <div
+              className={`swiper-slide w-1/2 flex justify-center items-center relative z-10 cursor-pointer ${
+                !isSecondSlide ? "text-black" : "text-white"
+              }`}
+              onClick={() => handleSlideClick(1)}
+            >
+              Client
+            </div>
+            <div
+              className={`swiper-slide w-1/2 flex justify-center items-center relative z-10 cursor-pointer ${
+                isSecondSlide ? "text-black" : "text-white"
+              }`}
+              onClick={() => handleSlideClick(2)}
+            >
+              Influencer
+            </div>
           </div>
           <div className="inputs flex justify-between gap-[20px]">
             <FloatingInput
               id="name"
               label="Name"
-              containerClassName="mt-8 flex-1"
+              containerClassName="mt-8 flex-1 hidden md:block"
             />
             <FloatingInput
               id="email"
@@ -86,7 +117,7 @@ const ContactUs = (className) => {
               containerClassName="mt-8 flex-1"
             />
           </div>
-          <button className="w-full bg-[#303134] rounded-full py-[15px] text-[20px]">
+          <button className="w-full bg-[#303134] uppercase rounded-full py-[15px] text-[16px] md:text-[20px]">
             Contact Us
           </button>
         </div>
@@ -95,7 +126,7 @@ const ContactUs = (className) => {
         <div className="flex w-full justify-between items-center gap-4 mb-4">
           <div className="h-[1px] w-[42%] bg-gray-400"></div>
 
-          <div className="title font-light text-center">
+          <div className="title font-light text-[14px] text-center">
             20+ Brands Trust Us
           </div>
 
--- src/pages/contact/Action.jsx
+++ src/pages/contact/Action.jsx
@@ -10,76 +10,83 @@ const Action = () => {
   };
 
   return (
-    <div data-scroll-section className="snap-start snap-always gap-[120px] h-screen flex py-20 px-14">
+    <div
+      data-scroll-section
+      className="snap-start snap-always gap-[120px] h-screen flex py-20 px-14"
+    >
       <div className="left-section w-1/2 h-full flex flex-col justify-start items-start">
-        <h2 className="subtitle h-[45px] text-[40px] ">Kick it off with Tikit!</h2>
-         <GradientText
-                      colors={["#40ffaa", "#4079ff", "#40ffaa", "#4079ff", "#40ffaa"]}
-                      animationSpeed={5}
-                      showBorder={false}
-                      className="title ml-0 text-[62px] font-bold"
-                    >
-                      Contact Us Now
-                    </GradientText>
-        
-        <p className="text-[24px] h-[58px] leading-[1] font-light ">We want to hear from you. let's us know how we can help!</p>
+        <h2 className="subtitle h-[45px] text-[40px] ">
+          Kick it off with Tikit!
+        </h2>
+        <GradientText
+          colors={["#40ffaa", "#4079ff", "#40ffaa", "#4079ff", "#40ffaa"]}
+          animationSpeed={5}
+          showBorder={false}
+          className="title ml-0 text-[62px] font-bold"
+        >
+          Contact Us Now
+        </GradientText>
+
+        <p className="text-[24px] h-[58px] leading-[1] font-light ">
+          We want to hear from you. let's us know how we can help!
+        </p>
       </div>
       <div className="right-section w-1/2 h-full flex flex-col gap-[40px]">
-          <div className="swiper-wrapper w-full border flex relative border-white h-[50px] rounded-full">
-             <div 
-               className={`move-item absolute w-1/2 h-full bg-white rounded-full transition-all duration-300 ease-in-out ${
-                 isSecondSlide ? 'left-1/2' : 'left-0'
-               }`}
-             />
-             <div 
-               className={`swiper-slide w-1/2 flex justify-center items-center relative z-10 cursor-pointer ${
-                 !isSecondSlide ? 'text-black' : 'text-white'
-               }`}
-               onClick={() => handleSlideClick(1)}
-             >
-               Client
-             </div>
-             <div 
-               className={`swiper-slide w-1/2 flex justify-center items-center relative z-10 cursor-pointer ${
-                 isSecondSlide ? 'text-black' : 'text-white'
-               }`}
-               onClick={() => handleSlideClick(2)}
-             >
-               Influencer
-             </div>
+        <div className="swiper-wrapper w-full border flex relative border-white h-[50px] rounded-full">
+          <div
+            className={`move-item absolute w-1/2 h-full bg-white rounded-full transition-all duration-300 ease-in-out ${
+              isSecondSlide ? "left-1/2" : "left-0"
+            }`}
+          />
+          <div
+            className={`swiper-slide w-1/2 flex justify-center items-center relative z-10 cursor-pointer ${
+              !isSecondSlide ? "text-black" : "text-white"
+            }`}
+            onClick={() => handleSlideClick(1)}
+          >
+            Client
           </div>
-          <div className="inputs  grid-rows-3 gap-[30px] grid grid-cols-2 flex-1">
-            <FloatingInput
-              id="contact-standard"
-              label="Name"
-              containerClassName="col-span-1 my-6 row-span-1"
-            />
-            <FloatingInput
-              id="contact-standard"
-              label="Email"
-              containerClassName="col-span-1 my-6 row-span-1 "
-            />
-            <FloatingInput
-              id="contact-standard"
-              label="Phone"
-              containerClassName="col-span-1 my-6 row-span-1"
-            />
-            <FloatingInput
-              id="contact-standard"
-              label="Subject"
-              containerClassName="col-span-1 my-6 row-span-1 "
-            />
-            
-<button
-className="px-5 h-[50px] cursor-pointer py-2.5 relative col-span-2 row-span-1 rounded-full group overflow-hidden font-medium bg-transparent text-white border border-white flex items-center justify-center">
-<span
-className="absolute top-0 left-0 flex w-full h-0 mb-0 transition-all duration-200 ease-out rounded-full transform translate-y-0 bg-white group-hover:h-full opacity-90"></span>
-<span className="relative uppercase group-hover:text-black text-base font-semibold">Contact Us</span>
-</button>
-            {/* <button className=" col-span-2 row-span-1 rounded-full border h-[50px] border-white ">
+          <div
+            className={`swiper-slide w-1/2 flex justify-center items-center relative z-10 cursor-pointer ${
+              isSecondSlide ? "text-black" : "text-white"
+            }`}
+            onClick={() => handleSlideClick(2)}
+          >
+            Influencer
+          </div>
+        </div>
+        <div className="inputs  grid-rows-3 gap-[30px] grid grid-cols-2 flex-1">
+          <FloatingInput
+            id="contact-standard"
+            label="Name"
+            containerClassName="col-span-1 my-6 row-span-1"
+          />
+          <FloatingInput
+            id="contact-standard"
+            label="Email"
+            containerClassName="col-span-1 my-6 row-span-1 "
+          />
+          <FloatingInput
+            id="contact-standard"
+            label="Phone"
+            containerClassName="col-span-1 my-6 row-span-1"
+          />
+          <FloatingInput
+            id="contact-standard"
+            label="Subject"
+            containerClassName="col-span-1 my-6 row-span-1 "
+          />
+
+          <button className="px-5 h-[50px] cursor-pointer py-2.5 relative col-span-2 row-span-1 rounded-full group overflow-hidden font-medium bg-transparent text-white border border-white flex items-center justify-center">
+            <span className="absolute top-0 left-0 flex w-full h-0 mb-0 transition-all duration-200 ease-out rounded-full transform translate-y-0 bg-white group-hover:h-full opacity-90"></span>
+            <span className="relative uppercase group-hover:text-black text-base font-semibold">
+              Contact Us
+            </span>
+          </button>
+          {/* <button className=" col-span-2 row-span-1 rounded-full border h-[50px] border-white ">
 Contact Us
             </button> */}
-          </div>
+        </div>
       </div>
     </div>
   );
