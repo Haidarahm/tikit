33f113397fd55ffdd5254ed7e0e8706cc0ebdb94
Haidar <hadiarahmad421@gmail.com>
2025-10-05 00:11:59 -0700

    Enhance Services component: improve mobile view styling by adding conditional borders and animation effects, and refine layout for better visual presentation across different screen sizes.

--- src/pages/Home/Services.jsx
+++ src/pages/Home/Services.jsx
@@ -26,13 +26,29 @@ const demoItems = [
 const Services = () => {
   return (
     <div className="section mt-16 relative font-hero-light flex flex-col mx-auto  md:h-[70vh]   z-10 w-full justify-center">
-      <h1 className="text-center font-bold text-[18px] md:text-[32px] mb-[38px]">What We Do Best</h1>
-      <div className="hidden md:block" style={{ position: "relative" ,height:'100%'}} >
+      <h1 className="text-center font-bold text-[18px] md:text-[32px] mb-[38px]">
+        What We Do Best
+      </h1>
+      <div
+        className="hidden md:block"
+        style={{ position: "relative", height: "100%" }}
+      >
         <FlowingMenu items={demoItems} />
       </div>
       <div className="mobile-view flex flex-col md:hidden">
         {demoItems.map((item, index) => (
-          <div key={index} className="flex border-white border-b-2 pb-4 justify-center text-[20px]  mb-4">
+          <div
+            key={index}
+            className={`flex pb-4 justify-center text-[20px] mb-4 ${
+              index < demoItems.length - 1 ? "border-white border-b-2" : ""
+            }`}
+            data-aos="flip-up"
+            data-aos-delay={index * 150}
+            data-aos-duration="600"
+            data-aos-easing="ease-out-cubic"
+            data-aos-once="false"
+            data-aos-mirror="true"
+          >
             <h2 className="text-lg font-semibold mt-2">{item.text}</h2>
           </div>
         ))}
